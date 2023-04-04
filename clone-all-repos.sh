4fb9b80ed91d4c57e126cacf02a0011d93d376a9
Luciano trujillo <luciano.trujillo@innovid.com>
2023-04-04 19:55:55 -0300

    add script to clone all repos

---
+++ clone-all-repos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REPO_URLS=("git@github.com:fiufit-taller2-grupo5/mobile-app.git" "git@github.com:fiufit-taller2-grupo5/user-service.git" "git@github.com:fiufit-taller2-grupo5/database-schemas.git" "git@github.com:fiufit-taller2-grupo5/web-app.git" "git@github.com:fiufit-taller2-grupo5/training-service.git" "git@github.com:fiufit-taller2-grupo5/development-setup.git" "git@github.com:fiufit-taller2-grupo5/api-gateway.git")
+# Get the current directory
+CLONE_DIR=$(pwd)
+
+# Change to the target directory
+cd "${CLONE_DIR}" || exit
+
+# Clone each repository
+for repo_url in "${REPO_URLS[@]}"; do
+  echo "Cloning ${repo_url}"
+  git clone "${repo_url}"
+done
