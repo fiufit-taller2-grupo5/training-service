3e127950fc7e615e7f0bdbe40117432502e5feb7
Maria Vazquez Navarro <mvazquezn@fi.uba.ar>
2023-04-05 10:04:06 -0300

    updated pull-all-repos file to use ssh url

--- pull-all-repos.sh
+++ pull-all-repos.sh
@@ -10,8 +10,15 @@ for dir in "${current_dir}"/*; do
     
     # Check if it's a git repository
     if [ -d .git ]; then
-      echo "Pulling in $(basename "${dir}")"
-      git pull
+      # Get the SSH URL for the Git repository
+      git_ssh_url=$(git config --get remote.origin.url | sed 's/https:\/\/github\.com/git@github.com:/')
+
+      if [ -n "${git_ssh_url}" ]; then
+        echo "Pulling in $(basename "${dir}")"
+        git pull "${git_ssh_url}"
+      else
+        echo "Skipping $(basename "${dir}"), no SSH URL found"
+      fi
     else
       echo "Skipping $(basename "${dir}"), not a git repository"
     fi
@@ -20,3 +27,4 @@ for dir in "${current_dir}"/*; do
     cd "${current_dir}" || exit
   fi
 done
+
