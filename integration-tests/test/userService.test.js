93056f5ba12af4f2e479f0808da3d18c0b6b6417
Maria Vazquez Navarro <mvazquezn@fi.uba.ar>
2023-04-25 18:06:04 -0300

    added first integration test

--- integration-tests/test/userService.test.js
+++ integration-tests/test/userService.test.js
@@ -1,11 +1,16 @@
 const request = require('supertest');
+const expect = require('chai').expect;
 const apiGatewayHost = 'http://localhost:3000';
 
-describe('API Gateway tests', () => {
+describe('Health Check test', () => {
   it('GET health user service', async () => {
-    const response = await request(apiGatewayHost).get('/user-service/health');
-    expect(response.statusCode).toBe(200);
+    const response = await request(apiGatewayHost)
+      .get('/user-service/health-check')
+      .set('dev', 'true'); // add this line to set the Authorization header
+
+    expect(response.statusCode).to.be.equal(200);
   });
 
   // Add more tests for other endpoints
 });
+
