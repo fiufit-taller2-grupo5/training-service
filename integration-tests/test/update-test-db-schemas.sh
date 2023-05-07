d64fdaa2ddf422869ddbee643f3eb5b7134b5105
Luciano trujillo <luciano.trujillo@innovid.com>
2023-05-07 01:51:09 -0300

    integration tests completely working

--- integration-tests/test/update-test-db-schemas.sh
+++ integration-tests/test/update-test-db-schemas.sh
@@ -2,6 +2,8 @@
 
 cd ../
 pwd
+docker compose -f ./docker-compose.yml stop postgres-service
+docker compose -f ./docker-compose.yml rm -f postgres-service
 ./update-local-db-schema.sh -f integration-tests/test/docker-test-compose.yml --service user-service
 ./update-local-db-schema.sh -f integration-tests/test/docker-test-compose.yml --service training-service
 
